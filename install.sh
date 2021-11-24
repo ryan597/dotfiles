156ad680ce7cbfdc68dfdc4881b551e906159d8c
Ryan Smith <ryan.smith@ucdconnect.ie>
2021-11-24 13:02:09 +0000

    no longer run as sudo

--- install.sh
+++ install.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-sudo -u $USER 
-{
 echo Installer script for dotfiles\n
 echo copying files
-cp -r /home/$SUDO_USER/dotfiles/.config /home/$SUDO_USER/
-cp -r /home/$SUDO_USER/dotfiles/.vim /home/$SUDO_USER/
-cp /home/$SUDO_USER/dotfiles/.* /home/$SUDO_USER/
+cp -r /home/$USER/dotfiles/.config /home/$USER/
+cp -r /home/$USER/dotfiles/.vim /home/$USER/
+cp /home/$USER/dotfiles/.* /home/$USER/
 
 echo cloning repositories for vim plugins
-git clone https://github.com/itchyny/lightline.vim.git /home/$SUDO_USER/.vim/pack/plugins/start/lightline
-git clone https://github.com/frazrepo/vim-rainbow.git /home/$SUDO_USER/.vim/pack/plugins/start/rainbow
-git clone https://github.com/powerline/fonts.git /home/$SUDO_USER/dotfiles/fonts/powerlinefonts
+git clone https://github.com/itchyny/lightline.vim.git /home/$USER/.vim/pack/plugins/start/lightline
+git clone https://github.com/frazrepo/vim-rainbow.git /home/$USER/.vim/pack/plugins/start/rainbow
+git clone https://github.com/powerline/fonts.git /home/$USER/dotfiles/fonts/powerlinefonts
 
 echo installing patched fonts
-chmod +x /home/$SUDO_USER/dotfiles/fonts/powerlinefonts/install.sh
-/home/$SUDO_USER/dotfiles/fonts/powerlinefonts/install.sh
+chmod +x /home/$USER/dotfiles/fonts/powerlinefonts/install.sh
+/home/$USER/dotfiles/fonts/powerlinefonts/install.sh
 
 echo installing fish and rust commands
-}
 
-apt install fish bat exa fd-find powerline fonts-powerline curl
+sudo apt install fish bat exa fd-find powerline fonts-powerline curl
 
-sudo -u $SUDO_USER 
-{
 chsh -s /usr/bin/fish
 
 echo installing oh-my-fish and bobthefish theme
@@ -32,9 +27,9 @@ omf install bobthefish
 
 echo replacing segment seperators in prompt
 sed -i '353 c\
-            echo -ns \\\ue0b0 " "' /home/$SUDO_USER/.local/share/omf/themes/bobthefish/functions/fish_prompt.fish
+            echo -ns \\\ue0b0 " "' /home/$USER/.local/share/omf/themes/bobthefish/functions/fish_prompt.fish
 sed -i '393 c\
-        echo -ns \\\ue0b0 " "' /home/$SUDO_USER/.local/share/omf/themes/bobthefish/functions/fish_prompt.fish
+        echo -ns \\\ue0b0 " "' /home/$USER/.local/share/omf/themes/bobthefish/functions/fish_prompt.fish
 
 if which conda
 then
@@ -43,4 +38,3 @@ then
 fi
 
 echo Install finished
-}
