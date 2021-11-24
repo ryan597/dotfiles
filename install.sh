3c576a9e4a5f336077890f6baf16a779f4b15bb6
Ryan Smith <ryan.smith@ucdconnect.ie>
2021-11-24 13:15:22 +0000

    pacman and apt installs

--- install.sh
+++ install.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-echo Installer script for dotfiles\n
-echo copying files
+echo "---------- Installer script for dotfiles ----------"
+echo "copying files"
 cp -r /home/$USER/dotfiles/.config /home/$USER/
 cp -r /home/$USER/dotfiles/.vim /home/$USER/
 cp /home/$USER/dotfiles/.* /home/$USER/
 
-echo cloning repositories for vim plugins
+echo "cloning repositories for vim plugins"
 git clone https://github.com/itchyny/lightline.vim.git /home/$USER/.vim/pack/plugins/start/lightline
 git clone https://github.com/frazrepo/vim-rainbow.git /home/$USER/.vim/pack/plugins/start/rainbow
 git clone https://github.com/powerline/fonts.git /home/$USER/dotfiles/fonts/powerlinefonts
 
-echo installing patched fonts
+echo "installing patched fonts"
 chmod +x /home/$USER/dotfiles/fonts/powerlinefonts/install.sh
 /home/$USER/dotfiles/fonts/powerlinefonts/install.sh
 
-echo installing fish and rust commands
+echo "installing fish and rust commands"
+echo "Enter password for sudo"
 
-sudo apt install fish bat exa fd-find powerline fonts-powerline curl
+if [ which apt ]; then
+    sudo apt install fish bat exa fd-find powerline fonts-powerline curl
+elif [ which pacman ]; then
+    sudo pacman -Sy fish bat exa fd-find powerline fonts-powerline curl
+else
+    echo "COULD NOT INSTALL FISH"
+    echo "Exiting..."
+    exit 1
+fi
 
-chsh -s /usr/bin/fish
+ln -s /usr/bin/fish /bin/fish
+chsh -s /bin/fish
 
-echo installing oh-my-fish and bobthefish theme
+echo "installing oh-my-fish and bobthefish theme"
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 omf install bobthefish
 
@@ -31,10 +41,9 @@ sed -i '353 c\
 sed -i '393 c\
         echo -ns \\\ue0b0 " "' /home/$USER/.local/share/omf/themes/bobthefish/functions/fish_prompt.fish
 
-if which conda
-then
+if [ which conda ]; then
     echo initialising conda for fish
     conda init fish
 fi
 
-echo Install finished
+echo "---------- Install finished ----------"
